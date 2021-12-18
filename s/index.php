f30c579ac7338706bd1f3324391b980b17637775
Dipak Koley <dipakkoley973@gmail.com>
2021-12-18 06:49:45 +0530

    improve short to long

--- s/index.php
+++ s/index.php
@@ -5,20 +5,24 @@
 	{
 		echo "<br><br><br><br><h1><font color='red'><center>ERROR 404!</center></font></h1>";
 	}
-	$sql = "SELECT * FROM urls where small=".$q.";";
-	$result=mysqli_query($conn,$sql);
-	if(!$result)
-	{
-		die(mysqli_error($conn));
-	}
-	$num=mysqli_num_rows($result);
-	if($num>0)
-	{
-        $row=mysqli_fetch_assoc($result);
-		header("Location:". $row['big']);
-	}
 	else
 	{
-		echo "<br><br><br><br><h1><font color='red'><center>ERROR 404!</center></font></h1>";
+		$sql = "SELECT * FROM urls where small=".$q.";";
+		$result=mysqli_query($conn,$sql);
+		if(!$result)
+		{
+			die(mysqli_error($conn));
+		}
+		$num=mysqli_num_rows($result);
+		if($num>0)
+		{
+			$row=mysqli_fetch_assoc($result);
+			header("Location:". $row['big']);
+		}
+		else
+		{
+			echo "<br><br><br><br><h1><font color='red'><center>ERROR 404!</center></font></h1>";
+		}
 	}
+	
 ?>
