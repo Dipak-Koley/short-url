8f95b548218793cc79e4231c9c065590ac3e7d8a
Dipak Koley <dipakkoley973@gmail.com>
2021-12-18 06:44:09 +0530

    improve short to long

--- connection.php
+++ connection.php
@@ -9,20 +9,4 @@
 	{
 		die("sorry we failed to connect:".mysqli_connect_error());
 	}
-	else
-	{
-		echo "connection was succesful";
-	}
-	$sql = "SELECT * FROM users;";
-	$result=mysqli_query($conn,$sql);
-	if(!$result)
-	{
-		die(mysqli_error($conn));
-	}
-	/*$num=mysqli_num_rows($result);
-	if($num>0)
-	{
-        $row=mysqli_fetch_assoc($result);
-	    echo var_dump($row);
-	}*/
 ?>
