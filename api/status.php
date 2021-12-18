ecfcd3954b61bab4f62d6846df0f18a4c3525125
Dipak Koley <dipakkoley973@gmail.com>
2021-12-18 20:48:17 +0530

    Status api created

---
+++ api/status.php
@@ -0,0 +1,16 @@
+<?php
+    require('../connection.php');
+    $sql="select * from urls";
+    $res=mysqli_query($con,$sql);
+    $count=mysqli_num_rows($res);
+    if($count>0)
+    {
+        while($row=mysqli_fetch_assoc($res)){
+            $arr[]=$row;
+        }
+        echo json_encode($['status'=>true,'data'=>$array,'result'=>'found']);
+    }
+    else{
+        echo json_encode($['status'=>true,'data'=>$array,'No data found'=>'found']);
+    }
+?>
