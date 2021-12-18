0a255a407f96f4f718d57ea9b455f7b35bd6e5fa
Dipak Koley <dipakkoley973@gmail.com>
2021-12-18 06:59:14 +0530

    improve short to long

--- s/index.php
+++ s/index.php
@@ -1,12 +1,12 @@
 <?php 
 	require('../connection.php');
-	$q=$_GET['q'];
 	if(!isset($_GET['q']))
 	{
 		echo "<br><br><br><br><h1><font color='red'><center>ERROR 404!</center></font></h1>";
 	}
 	else
 	{
+		$q=$_GET['q'];
 		$sql = "SELECT * FROM urls where small=".$q.";";
 		$result=mysqli_query($conn,$sql);
 		if(!$result)
@@ -17,6 +17,7 @@
 		if($num>0)
 		{
 			$row=mysqli_fetch_assoc($result);
+			$sql="UPDATE urls SET count=2 WHERE small=".$q.";";
 			header("Location:". $row['big']);
 		}
 		else
