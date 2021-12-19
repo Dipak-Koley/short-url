49573300f164ab34586b76dec8eae490c2237c8c
Dipak Koley <dipakkoley973@gmail.com>
2021-12-19 11:32:13 +0530

    Login functionality improve

--- api/login.php
+++ api/login.php
@@ -7,7 +7,8 @@
     $check=mysqli_num_rows(mysqli_query($conn,"select * from users where user_id='$user_id'"));
     if($check==0)
     {
+        echo "done";
         mysqli_query($conn,"insert into users(name,email,image,user_id) values('$name','$email','$image','$user_id')");
     }
-    echo "done";
+    
 ?>
