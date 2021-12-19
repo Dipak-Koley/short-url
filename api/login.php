becfb1aed0d6f28a69356809d61a554d7348097f
Dipak Koley <dipakkoley973@gmail.com>
2021-12-19 12:55:31 +0530

    Login functionality improve

--- api/login.php
+++ api/login.php
@@ -1,6 +1,7 @@
 <?php
     require('../connection.php');
-    $name=mysqli_real_escape_string($conn,$_POST['name']);
+    //$name=mysqli_real_escape_string($conn,$_POST['name']);
+    $name=$_POST['name'];
     $email=mysqli_real_escape_string($conn,$_POST['email']);
     $user_id=mysqli_real_escape_string($conn,$_POST['user_id']);
     $image=mysqli_real_escape_string($conn,$_POST['image']);
