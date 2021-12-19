5241e8fdc6545e0893cb2c66a3dfb4e094eeba49
Dipak Koley <dipakkoley973@gmail.com>
2021-12-19 09:00:48 +0530

    Login View Improve

--- view/js/main.js
+++ view/js/main.js
@@ -2,3 +2,7 @@ function gmailLogIn(userInfo){
     var userProfile=userInfo.getBasicProfile();
     console.log(userProfile)
 }
+function logout(){
+    var auth = gapi.auth2.getAuthInstance();
+    auth.signOut();
+}
