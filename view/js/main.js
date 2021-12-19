bbf0bf9b1d0309f8eb606e4bbe5651a32429c0a9
Dipak Koley <dipakkoley973@gmail.com>
2021-12-19 09:17:58 +0530

    Login functionality improve

--- view/js/main.js
+++ view/js/main.js
@@ -1,6 +1,9 @@
 function gmailLogIn(userInfo){
     var userProfile=userInfo.getBasicProfile();
-    console.log(userProfile)
+    console.log(userProfile.getId());
+    console.log(userProfile.getName());
+    console.log(userProfile.getImageUrl());
+    console.log(userProfile.getEmail());
 }
 function logout(){
     var auth = gapi.auth2.getAuthInstance();
