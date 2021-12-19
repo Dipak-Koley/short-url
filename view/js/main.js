0061095c0f2472e339baf7d99930e1f438d37a1a
Dipak Koley <dipakkoley973@gmail.com>
2021-12-19 10:38:40 +0530

    Login functionality improve

--- view/js/main.js
+++ view/js/main.js
@@ -3,7 +3,7 @@ function gmailLogIn(userInfo){
     jQuery.ajax({
         url:'../api/login.php',
         type: 'post',
-        data: 'user_id='+userProfile.getId()+'&name='+userProfile.getName()+'image=' +userProfile.getImageUrl()+'email=' +userProfile.getEmail(),
+        data: 'user_id='+userProfile.getId()+'&name='+userProfile.getName()+'&image=' +userProfile.getImageUrl()+'&email=' +userProfile.getEmail(),
         success: function(result){
             console.log(result)
         }
