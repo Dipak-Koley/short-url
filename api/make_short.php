d433b158851d30f9921b54f535cb5277cc6fd718
Dipak Koley <dipakkoley973@gmail.com>
2021-12-20 23:42:55 +0530

    improved dashboard

--- api/make_short.php
+++ api/make_short.php
@@ -1,17 +1,17 @@
 <?php
 require('../connection.php');
 session_start();
+echo "Please Wait...";
 $urls = explode(',', $_POST['url']);
   foreach ($urls as $i) 
   {
-    echo $i;
-    echo " ";
     $k=mysqli_real_escape_string($conn,$i);
     $result=mysqli_query($conn,"select * from urls where big like '$k'");
     $check=mysqli_num_rows($result);
     if($check==0)
     {
         mysqli_query($conn,"INSERT INTO urls(big,user_id,count) VALUES ('".$i."','".$_SESSION['user_id']."','0')");
-    }
+        header("Location: ../view/status.php");
+      }
   }
 ?>
