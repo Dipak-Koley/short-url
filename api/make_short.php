f9af4be2835a59745119a38dcb51f497aadb8f01
Dipak Koley <dipakkoley973@gmail.com>
2021-12-20 23:21:57 +0530

    improved dashboard

--- api/make_short.php
+++ api/make_short.php
@@ -1,8 +1,13 @@
 <?php
 echo $_POST['url'];
-$array = explode(',', $_POST['url']);
-for ($x = 0; $x < count($array); $x++) {
+$urls = explode(',', $_POST['url']);
+/*for ($x = 0; $x < count($array); $x++) {
     echo $array." ";
-  }
+  }*/
+  foreach ($urls as $i) {
+  echo $i;
+  echo " ";
+
+}
 
 ?>
