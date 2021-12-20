0d8f47ee2fb9f726d573d71bdcb16d003820dd20
Dipak Koley <dipakkoley973@gmail.com>
2021-12-20 17:46:48 +0530

    improved dashboard

--- view/status.php
+++ view/status.php
@@ -1,6 +1,21 @@
+<html>
+    <head>
+            <link rel="stylesheet" href="./css/status.css">
+    </head>
+<body>
+
+<center><h1><font color="#b50505"><b>Dashboard</b></font></h1></center>
+<table id="status_table">
+  <tr>
+    <th>Original</th>
+    <th>Short</th>
+    <th>Hit</th>
+  </tr>
+
 <?php
     session_start();
     require('../connection.php');
+    $base="18.221.4.188/w/s/?q=";
     $sql = "SELECT * FROM urls where user_id=".$_SESSION['user_id']." order by small desc;";
     $result=mysqli_query($conn,$sql);
     if(!$result)
@@ -11,41 +26,20 @@
     if($num>0)
     {
         while($row=mysqli_fetch_assoc($result)){
-            $array[]=$row;
+            //$array[]=$row;
+            echo "<tr>
+            <td>".$row['big']."</td>
+            <td>".$base.$row['small']."</td>
+            <td>".$row['count']."</td>
+          </tr>";
         }
     }
 ?>
-<html>
-    <head>
-            <link rel="stylesheet" href="./css/status.css">
-    </head>
-<body>
 
-<center><h1><font color="#b50505"><b>Dashboard</b></font></h1></center>
+</table>
 <?php echo "result ";
   echo json_encode(['status'=>true,'data'=>$array,'result'=>'found']);
 ?>
-<table id="status_table">
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Avul Pakir Jainsation (DRDO) and Indian</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Avul Pakir Jainularganisation (DRDO) and Indian</td>
-    <td>Christina Berglund</td>
-    <td>Sweden</td>
-    <td>Germany</td>
-  </tr>
-</table>
-
 </body>
 </html>
 
