339eb097715cd350f6a5fa53ca657dd43055e67f
Dipak Koley <dipakkoley973@gmail.com>
2021-12-20 23:32:53 +0530

    improved dashboard

--- api/make_short.php
+++ api/make_short.php
@@ -1,13 +1,16 @@
 <?php
-echo $_POST['url'];
+require('../connection.php');
+session_start();
 $urls = explode(',', $_POST['url']);
-/*for ($x = 0; $x < count($array); $x++) {
-    echo $array." ";
-  }*/
-  foreach ($urls as $i) {
-  echo $i;
-  echo " ";
-
-}
-
+  foreach ($urls as $i) 
+  {
+    echo $i;
+    echo " ";
+    $result=mysqli_query($conn,"select * from urls where big like".$i);
+    $check=mysqli_num_rows($result);
+    if($check==0)
+    {
+        mysqli_query($conn,"INSERT INTO urls(big,user_id,count) VALUES ('".$i."','".$_SESSION['user_id']."','0')");
+    }
+  }
 ?>
